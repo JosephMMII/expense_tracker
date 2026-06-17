8a48d1d29619ef6e548149e219ef2483272bee72
Broly <joe.m.munger@gmail.com>
2026-06-16 20:00:50 -0600

    new commit

---
+++ func.py
@@ -0,0 +1,33 @@
+from main.py import category
+
+class Category:
+
+    categories = [
+        "Utilities", "Transportation", "Food", "Insurance",
+        "Entertainment", "Personal Care", "Credit"
+    ]
+
+    def __init__(self):
+        self.category_tracker = {cat: [] for cat in self.categories}
+
+    def add_to_cat(self, num):
+        while True:
+            cat = self.categories[(num + 1) % len(self.categories)]
+
+            try:
+                amount = float(input(f"Amount for {cat}: "))
+                self.category[cat].append(amount)
+            except ValueError as e:
+                print(f"Invalid entry: ({e})")
+                continue
+            
+            try:
+                cont = input(f"Add more to '{cat}'? (Y/N): ")
+                if str(cont.lower()) == "y":
+                    continue
+                else:
+                    return False
+            except ValueError as e:
+                print(f"Invalid entry ({e})")
+                continue
+            
---
+++ test.py
@@ -0,0 +1,172 @@
+import sys
+from func import Category as C
+
+category = {
+    "Utilities": [],
+    "Transportation": [],
+    "Food": [],
+    "Insurance": [],
+    "Entertainment": [],
+    "Personal Care": [],
+    "Credit": []
+}
+
+while True:
+    print("\n==== Expense Tracker ====\n")
+    print("1. Add Expense\n2. View Expenses\n3. View Total Spending")
+    print("4. Search by Category\n5. Save Expenses")
+    print("6. Load Expenses\n7. Exit")
+
+    try:
+        choice = int(input("Select category: "))
+    except ValueError as e:
+        print(f"Invalid entry: ({e})")
+        continue
+
+    if choice not in range(1,8):
+        print("Invalid entry")
+        continue
+    elif choice == 7:
+        sys.exit()
+
+    if choice == 1:
+        print("\nWhat category?")
+        print("1. Utilities\n2. Transportation\n3. Food\n4. Insurance\n5. Entertainment\n6. Personal Care\n7. Credit")
+        
+        try:
+            cat = int(input("\nEnter number: "))
+        except Exception as e:
+            print(f"Invalid entry: ({e})")
+            continue
+
+        if cat not in range(1,8):
+            print("Invalid entry. Number must be between 1-7.")
+            continue
+
+        while True:
+            var = C.add_to_cat(cat)
+
+            if var == False:
+                break
+
+
+
+
+
+
+
+            # if cat == 1:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Utilities"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Utilities'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            # elif cat == 2:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Transportation"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Transportation'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            # elif cat == 3:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Food"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Food'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            # elif cat == 4:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Insurance"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Insurance'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            # elif cat == 5:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Entertainment"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Entertainment'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            # elif cat == 6:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Personal Care"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Personal Care'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            # elif cat == 7:
+            #     try:
+            #         num = float(input("Amount: "))
+            #         category["Credit"].append(num)
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+            #         continue
+
+            #     try:
+            #         cont = str(input("Want to add more to 'Credit'? (Y/N): "))
+            #         if cont.lower() == "y":
+            #             continue
+            #         else:
+            #             break
+            #     except ValueError as e:
+            #         print(f"Invalid entry: ({e})")
+
+            break
+    break
